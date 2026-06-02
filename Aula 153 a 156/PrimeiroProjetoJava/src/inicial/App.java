a92b9328427b09688d5ed35c0d08bec2b7abab5d
edimilsonjr-nuvem <jr.edimilson460@gmail.com>
2026-06-02 16:50:24 -0300

    Add files via upload

---
+++ Aula 153 a 156/PrimeiroProjetoJava/src/inicial/App.java
@@ -0,0 +1,23 @@
+package inicial;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Edimilson");
+		
+		String nome = "Edimilson";
+		int idade = 18;
+		
+		System.out.println("Olá, Mundo!!");
+		System.out.print("Seja bem-vindo " + nome + ". ");
+		System.out.print("Essa é a sua primeira aula de Java!!");
+		
+		System.out.printf("\nMeu nome é %s e tenho %d anos.\n\n", nome, idade);
+		
+		double pi = 3.1415926536;
+		
+		System.out.printf("O valor de pi é %.2f", pi);
+
+	}
+
+}
